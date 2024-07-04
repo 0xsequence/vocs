e5a4f08955a9f897696239e91065d4b578532586
JamesLawton <crossoveranx@gmail.com>
2024-07-04 18:19:14 +0000

    Build: (a851930) Update on-push-to-main.yml

---
+++ _lib/cli/version.d.ts
@@ -0,0 +1,2 @@
+export declare const version = "1.0.0-alpha.51";
+//# sourceMappingURL=version.d.js.map
---
+++ _lib/cli/version.js
@@ -0,0 +1,2 @@
+export const version = '1.0.0-alpha.51';
+//# sourceMappingURL=version.js.map
